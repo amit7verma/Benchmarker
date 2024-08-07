441b5dc8568cbffc94223d87db6bc8f240880f55
Amit Kumar <amit7verma@gmail.com>
2024-08-07 12:49:43 +0530

    completed the implementation

---
+++ BenchmarkResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace Benchmarker
+{
+    public class BenchmarkResult
+    { 
+        public long[] MilliSeconds { get; internal set; } = Array.Empty<long>();
+        public double Average { get; internal set; }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            var idx = 1;
+            foreach (var item in MilliSeconds)
+            {
+                sb.AppendLine($"Trial {idx}: {item} ms");
+                idx++;
+            }
+            sb.AppendLine($"Average: {Average} ms");
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ Benchmarker.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Benchmarker
+{
+    public class Benchmarker
+    {
+        public static BenchmarkResult CollectResult<T>(int attemptCount, Func<T> work)
+        {
+            var stopWatch = new Stopwatch();
+            var timesheet = new List<long>();
+            for (var i = 0; i < attemptCount; i++)
+            { 
+                stopWatch.Restart();
+                _ = work();
+                stopWatch.Stop();
+                timesheet.Add(stopWatch.ElapsedMilliseconds);
+                stopWatch.Reset();
+            }
+
+            return new BenchmarkResult() 
+            { 
+                MilliSeconds = timesheet.ToArray(),
+                Average = timesheet.Average()
+            };
+        }
+
+        public static BenchmarkResult CollectResult(int attemptCount, Action work)
+        {
+            var stopWatch = new Stopwatch();
+            var timesheet = new List<long>();
+            for (var i = 0; i < attemptCount; i++)
+            {
+                stopWatch.Restart();
+                work();
+                stopWatch.Stop();
+                timesheet.Add(stopWatch.ElapsedMilliseconds);
+                stopWatch.Reset();
+            }
+
+            return new BenchmarkResult()
+            {
+                MilliSeconds = timesheet.ToArray(),
+                Average = timesheet.Average()
+            };
+        }
+    }
+}
